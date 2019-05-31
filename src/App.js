2ebce604671b7cbaf25e8b114016f927524b365f
Rostyan <kondrat1310@gmail.com>
2019-05-31 16:26:01 +0300

    added form mark up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Form from './component/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Form />
     </div>
   );
 }
